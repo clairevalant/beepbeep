01762a6c9481e57f27d55fa3da5b248e7551c73b
clairevalant <valantclaire@gmail.com>
2019-01-27 16:34:48 -0500

    added Google auth instead of GH, added user specific  rendering of header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+// Claire Valant's Tread Tech Challenge
+// January 27, 2019
+
 import React, { Component } from 'react';
 import firebase from 'firebase';
 import './App.css';
-import { throws } from 'assert';
 
 // Initialize Firebase
 var config = {
@@ -14,8 +16,8 @@ var config = {
 };
 firebase.initializeApp(config);
 
-// specify we want to use GitHub to provide authentication
-const provider = new firebase.auth.GithubAuthProvider();
+// specify we want to use Google to provide authentication
+const provider = new firebase.auth.GoogleAuthProvider;
 const auth = firebase.auth();
 
 class App extends Component {
@@ -50,8 +52,16 @@ class App extends Component {
     });
   }
 
+// turn off event listener
+  componentWillUnmount() {
+    if (this.dbref) {
+      this.dbRef.off();
+    }
+  }
+
   logIn = () => {
     auth.signInWithPopup(provider).then((result => {
+      console.log(result);
       this.setState({
         user: result.user
       });
@@ -71,8 +81,23 @@ class App extends Component {
     return (
       <div className="App">
         <header>
-          <h1>Moodboard</h1>
+          <div className="wrapper">
+            {this.state.user ? <button onClick={this.logOut}>Logout</button> : <button onClick={this.logIn}>Login</button>}
+            {/* Use ternary operators to display the appropriate header info */}
+            <h1>Moodboard</h1>
+            {this.state.user ? <div className="welcomeMsg"><p>Welcome back to Moodboard, {this.state.user.displayName}</p><img className="profilePic" src={this.state.user.photoURL} alt="your Google photo" /></div>
+            : <p>Welcome to Moodboard. Post your favourite images to create a visual diary of how you're feeling. Get started by logging in with Google.</p>}
+          </div>
         </header>
+
+        {this.state.user ? 
+          <main>
+            
+          </main>
+          : <main>
+
+          </main>
+        }
       </div>
     );
   }
